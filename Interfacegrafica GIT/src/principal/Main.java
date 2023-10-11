f756de37d38e6c9b1b32c3d8ed0ecf93c491da28
HigorPMorais <higorpinto901@gmail.com>
2023-10-10 21:36:15 -0300

    10/10 - 01

---
+++ Interfacegrafica GIT/src/principal/Main.java
@@ -0,0 +1,18 @@
+package principal;
+
+import visao.ListaAlunos;
+import visao.TelaPrincipal;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+        
+//        ListaAlunos telaAlunos = new ListaAlunos();
+//        telaAlunos.setVisible(true);
+    }
+    
+}
